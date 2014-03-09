c7079378b2b2bd201ad2d8e63828825827fe276c
Michael Brunton-Spall <michael@brunton-spall.co.uk>
2014-03-09 21:00:44 +0000

    First simple stab at moving around

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,18 +1,47 @@
-/**
- * Created by michaelbruntonspall on 09/03/2014.
- */
+import model._
+import model.Glyph
+import model.Ship
 import ncurses4j.NCurses
 object Main {
-  def main(args: Array[String]) {
-    val screen = NCurses.initscr()
-    val maxx = NCurses.getmaxx(screen)
-    val maxy = NCurses.getmaxy(screen)
-    val msg = "Testing curses display"
-    val startx = maxx/2 - msg.length/2
+  val world = new World()
+  val screen = NCurses.initscr()
+  val maxx = NCurses.getmaxx(screen)
+  val maxy = NCurses.getmaxy(screen)
+  val player = Ship("Player 1", Glyph('@', true, 0), Position(10,10))
+  world.addShip(player)
+
+  def renderScreen() {
     NCurses.erase()
-    NCurses.mvprintw(maxy/2, startx, msg)
+    world.systems.values.foreach(render)
+    world.ships.values.foreach(render)
+    NCurses.mvprintw(maxy,0,"[q]uit, [hjkl] move")
     NCurses.refresh()
-    NCurses.getch()
+  }
+
+  def renderShips() {
+  }
+
+  def render(r: Renderable) {
+    NCurses.mvprintw(r.position.y, r.position.x, r.glyph.chr.toString)
+  }
+
+  def main(args: Array[String]) {
+    var stop = false;
+    while (!stop) {
+      renderScreen()
+
+      val i = NCurses.getch()
+      i match {
+        case 10 => stop = true // Enter
+        case 27 => stop = true // Escape
+        case 113 => stop = true // q
+        case 104 => player.position.x -= 1// h
+        case 106 => player.position.y += 1// j
+        case 107 => player.position.y -= 1// k
+        case 108 => player.position.x += 1// l
+        case _ =>
+      }
+    }
     NCurses.endwin()
   }
 }
--- src/main/scala/model/Model.scala
+++ src/main/scala/model/Model.scala
@@ -1,11 +1,12 @@
 package model
 
-class Glyph(val chr: Char, val bold: Boolean, val colour: Integer) {
-
-}
+case class Glyph(chr: Char, bold: Boolean, colour: Integer)
 
 /** Not going for immutability, we can think if there's a way to approach immutability later */
 class Position(var x: Int, var y: Int)
+object Position {
+  def apply(x: Int, y: Int) = new Position(x,y)
+}
 
 class Renderable(val glyph: Glyph, val position: Position)
 
@@ -13,6 +14,12 @@ case class System(name: String, override val glyph: Glyph, override val position
 case class Ship(id: String, override val glyph: Glyph, override val position: Position) extends Renderable(glyph, position)
 
 class World {
-  def systems: Map[Position, System] = ???
-  def ships: Map[Position, Ship] = ???
+
+  var systems: Map[Position, System] = Map(
+    Position(2,2) -> System("Earth", Glyph('O', true, 0), Position(2,2)),
+    Position(21,17) -> System("Earth", Glyph('x', true, 0), Position(21,17))
+  )
+  var ships: Map[Position, Ship] = Map.empty[Position,Ship]
+
+  def addShip(ship: Ship) = ships = ships + (ship.position -> ship)
 }
